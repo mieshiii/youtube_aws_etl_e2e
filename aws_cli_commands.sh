7ddac09befb550e3fc5d4ddc497ebd762fba676d
Mies Dela Cruz <mieseliel.delacruz@gmail.com>
2023-01-18 23:47:27 +0800

    #4 - Refactor bash script

--- aws_cli_commands.sh
+++ aws_cli_commands.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 #Replace It With Your Bucket Name
-
+export bucket="e2e-youtube-data-analysis-raw-ap-southeast-1-dev"
 # To copy all JSON Reference data to same location:
-aws s3 cp . s3://{your_bucket}/youtube/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
+aws s3 cp . s3://$bucket/youtube/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
 
 # To copy all data files to its own location, following Hive-style patterns:
-aws s3 cp CAvideos.csv s3://{your_bucket}/youtube/raw_statistics/region=ca/
-aws s3 cp DEvideos.csv s3://{your_bucket}-dev/youtube/raw_statistics/region=de/
-aws s3 cp FRvideos.csv s3://{your_bucket}/youtube/raw_statistics/region=fr/
-aws s3 cp GBvideos.csv s3://{your_bucket}/youtube/raw_statistics/region=gb/
-aws s3 cp INvideos.csv s3://{your_bucket}/youtube/raw_statistics/region=in/
-aws s3 cp JPvideos.csv s3://{your_bucket}/youtube/raw_statistics/region=jp/
-aws s3 cp KRvideos.csv s3://{your_bucket}/youtube/raw_statistics/region=kr/
-aws s3 cp MXvideos.csv s3://{your_bucket}/youtube/raw_statistics/region=mx/
-aws s3 cp RUvideos.csv s3://{your_bucket}/youtube/raw_statistics/region=ru/
-aws s3 cp USvideos.csv s3://{your_bucket}/youtube/raw_statistics/region=us/
+aws s3 cp CAvideos.csv s3://$bucket/youtube/raw_statistics/region=ca/
+aws s3 cp DEvideos.csv s3://$bucket/youtube/raw_statistics/region=de/
+aws s3 cp FRvideos.csv s3://$bucket/youtube/raw_statistics/region=fr/
+aws s3 cp GBvideos.csv s3://$bucket/youtube/raw_statistics/region=gb/
+aws s3 cp INvideos.csv s3://$bucket/youtube/raw_statistics/region=in/
+aws s3 cp JPvideos.csv s3://$bucket/youtube/raw_statistics/region=jp/
+aws s3 cp KRvideos.csv s3://$bucket/youtube/raw_statistics/region=kr/
+aws s3 cp MXvideos.csv s3://$bucket/youtube/raw_statistics/region=mx/
+aws s3 cp RUvideos.csv s3://$bucket/youtube/raw_statistics/region=ru/
+aws s3 cp USvideos.csv s3://$bucket/youtube/raw_statistics/region=us/
 
